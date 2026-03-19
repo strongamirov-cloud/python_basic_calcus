7c9f312713b67c5ad08f1afd6e4f6c1d8e32f97a
Orifjon <strongamirov@gmail.com>
2026-03-19 23:49:41 +0500

    Create code.kt

---
+++ code.kt
@@ -0,0 +1,17 @@
+import java.util.Scanner
+
+//*fun main(args: Array<String>) {
+ //   println("Matin kiriting")
+ //var scanner = Scanner(System.`in`)
+   // print("Matn=")
+ //   var mat = scanner.next()
+ //   var a=arrayOf("code")
+  //   for (i in 0..mat.length) {
+    //     if (i!=3) {
+      //       if (mat[i] == a[i]) {
+
+        //     }
+    //     }
+      //   print("$i")
+//}
+//}
